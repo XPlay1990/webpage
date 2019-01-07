7a16189353dcf2bf20c7177fb45a02294e416b70
Jan Adamczyk <j_adamczyk@hotmail.com>
2019-01-07 21:30:18 +0100

    Initial commit

---
+++ src/test/java/com/jad/webpage/WebpageApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jad.webpage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebpageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
